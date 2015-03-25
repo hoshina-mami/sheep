350d59bb962e7fdf3646379a7778f5263a828977
hoshina_mami <hoshina_mami@cyberagent.co.jp>
2015-03-25 19:58:28 +0900

    UIRootの設定とcameraのSizeを設定

--- sheep4/Assets/Scripts/Title.cs
+++ sheep4/Assets/Scripts/Title.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Title : MonoBehaviour {
 	
 	//private
+	private GameObject TitleScript;
 	private GameObject Background;
 	private GameObject Logo;
 	private GameObject Btn_start;
@@ -15,11 +16,12 @@ public class Title : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		 
-		Background = GameObject.Find("Background");
-		Logo       = GameObject.Find("Logo");
-		Btn_start  = GameObject.Find("Btn_start");
-		Btn_record = GameObject.Find("Btn_record");
-		Btn_option = GameObject.Find("Btn_option");
+		TitleScript = GameObject.Find("TitleScript");
+		Background  = GameObject.Find("Background");
+		Logo        = GameObject.Find("Logo");
+		Btn_start   = GameObject.Find("Btn_start");
+		Btn_record  = GameObject.Find("Btn_record");
+		Btn_option  = GameObject.Find("Btn_option");
 	
 	}
 	
@@ -30,21 +32,26 @@ public class Title : MonoBehaviour {
 	
 	// called from onRelease Event of GameObject"Btn_start"
 	void ClickStartBtn () {
+		//makes object's alpha 0
 		Logo.GetComponent<TweenAlpha> ().enabled = true;
 		Btn_start.GetComponent<TweenAlpha> ().enabled = true;
 		Btn_record.GetComponent<TweenAlpha> ().enabled = true;
 		Btn_option.GetComponent<TweenAlpha> ().enabled = true;
 		Background.GetComponent<TweenPosition> ().enabled = true;
 
+		//remove clouds
 		GameObject[] clouds = GameObject.FindGameObjectsWithTag("cloud");
 		foreach(GameObject cloud in clouds) {
 			Destroy(cloud);
 		}
 
+		//remove sheeps
 		GameObject[] sheeps = GameObject.FindGameObjectsWithTag("sheep");
 		foreach(GameObject sheep in sheeps) {
 			Destroy(sheep);
 		}
+
+		//TODO stop to create sheeps
 	}
 
 	// called from TweenPosition of GameObject"Background"
