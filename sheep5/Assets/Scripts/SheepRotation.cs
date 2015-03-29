c11cfebfe74788a1f89534e7d02dff63d73a024d
hoshinamami <brighter722@gmail.com>
2015-03-29 21:57:16 +0900

    [title] uguiTweenでひつじを1匹ころがした

---
+++ sheep5/Assets/Scripts/SheepRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepRotation : MonoBehaviour {
+
+	private GameObject  sheep;
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+		sheep = this.gameObject;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotateSheep(sheep);
+	}
+	
+	void RotateSheep(GameObject target)
+	{
+		Quaternion rotation = Quaternion.AngleAxis(speed, Vector3.forward);
+		// 回転
+		target.transform.rotation =  target.transform.rotation * rotation;
+	}
+}
---
+++ sheep5/Assets/Scripts/TitleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleController : MonoBehaviour {
+
+	//private
+	private GameObject TitleControllerObj;
+	private GameObject Background;
+	private GameObject Logo;
+	private GameObject Btn_start;
+	private GameObject Btn_record;
+	private GameObject Btn_option;
+
+	// Use this for initialization
+	void Start () {
+
+		TitleControllerObj = GameObject.Find("TitleControllerObj");
+		Background         = GameObject.Find("Background");
+		Logo               = GameObject.Find("Logo");
+		Btn_start          = GameObject.Find("Btn_start");
+		Btn_record         = GameObject.Find("Btn_record");
+		Btn_option         = GameObject.Find("Btn_option");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
